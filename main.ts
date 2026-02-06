1832aef54500a8bd9e4be0a3d36495d4273f7049
ysaitozzz <yksaito02@gmail.com>
2026-02-06 17:27:26 +0900

    feat: ログ出力を行うシンプルなmain.tsとGitHub Actionsを追加

---
+++ main.ts
@@ -0,0 +1,4 @@
+console.log("Deno is running on GitHub Actions");
+console.log(`Deno version: ${Deno.version.deno}`);
+console.log(`TypeScript version: ${Deno.version.typescript}`);
+console.log(`V8 version: ${Deno.version.v8}`);
